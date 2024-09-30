cdc377c3f596426e4e292e19f11f20022c30de47
Alberto <92520673+albertoPicasso@users.noreply.github.com>
2024-09-30 14:39:09 +0200

    new desing Final version

--- viewAgent/static/js/darkMode.js
+++ viewAgent/static/js/darkMode.js
@@ -1,4 +1,10 @@
 // darkMode.js
+
+// Cambiar el modo oscuro al hacer clic en el botón
+document.getElementById('toggle-dark-mode').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const toggleDarkModeButton = document.getElementById('toggle-dark-mode');
     let isDarkMode = false;
